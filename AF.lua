3240f01eca04efa1ba2a1aa86b60bed2520a3b0c
1XonoX1 <paratirar2022@gmail.com>
2025-07-19 15:55:53 +0200

    Update AF.lua

--- AF.lua
+++ AF.lua
@@ -1,4 +1,4 @@
-local ShouldReel = false
+local WindUI = loadstring(game:HttpGet("https://github.com/Footagesus/WindUI/releases/latest/download/main.lua"))()
 
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
