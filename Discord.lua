2b00435d98b785a14cd3b2e288870207ed4b0ab4
1XonoX1 <paratirar2022@gmail.com>
2025-07-23 12:54:49 +0200

    Create Discord.lua

---
+++ Discord.lua
@@ -0,0 +1,36 @@
+local DefaultWebhookAuthor = "AutoFish | By XonoX"
+
+local HttpService = game:GetService("HttpService")
+
+local Request = http_request or request or (http and http.request)
+
+if not Request then
+    getgenv().WindUI:Notify({
+        Title = "There is no available library for HTTP requests, Discord webhooks will be disabled",
+        Duration = 5
+    })
+end
+
+getgenv().SendWebhook = function ()
+    if not getgenv().DiscordWebhookToggle.Value then
+        return
+    end
+
+    local JSONData = HttpService:JSONEncode({
+        content = "Caught: " .. LastCatch,
+        username = DefaultWebhookAuthor
+    })
+
+    if Request then
+        local Response = Request({
+            Url = getgenv().DiscordWebhookTokenInput.Value,
+            Method = "POST",
+            Headers = {
+                ["Content-Type"] = "application/json"
+            },
+            Body = JSONData
+        })
+
+        print("AutoFish | Webhook message sent. Status code:", Response.StatusCode)
+    end
+end
